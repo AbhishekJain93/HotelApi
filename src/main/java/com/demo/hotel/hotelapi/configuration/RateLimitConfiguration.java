b373995a217c89d2b24e20868cfd62ca511a40eb
Abhishek Jain <abhishek.jain01@snapdeal.com>
2017-03-09 10:07:03 +0530

    Add synchronization for thread safety for hotel api and rate limiter

--- src/main/java/com/demo/hotel/hotelapi/configuration/RateLimitConfiguration.java
+++ src/main/java/com/demo/hotel/hotelapi/configuration/RateLimitConfiguration.java
@@ -19,6 +19,7 @@ import com.demo.hotel.hotelapi.model.RateLimitModel;
 public class RateLimitConfiguration {
 
 	static Logger log = Logger.getLogger(RateLimitConfiguration.class.getName());
+	private final Object lock = new Object();
 
 	@Value("#{${ratelimit.map}}")
 	private Map<String, String> apiRateMap = new HashMap<>();
@@ -55,65 +56,68 @@ public class RateLimitConfiguration {
 
 	public boolean incrementCounter(String apiName) {
 
-		RateLimitModel rateLimitModel = runtimeConfig.get(apiName);
-
-		if (rateLimitModel == null) {
-
-			log.info(" Api rate not configured for apiKey : " + apiName + ". So falling back to default rate : "
-					+ defaultRate);
-
-			rateLimitModel = new RateLimitModel();
-			rateLimitModel.currentCounter = 1;
-			rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
-			rateLimitModel.nextFrame = getNextTimeFrame(apiName, defaultRate);
-			runtimeConfig.put(apiName, rateLimitModel);
-			return true;
-
-		}
-
 		Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
-		if (rateLimitModel.nextFrame.before(currentTimestamp)) {
+		synchronized (lock) {
+			RateLimitModel rateLimitModel = runtimeConfig.get(apiName);
 
-			log.info(" Api called after the time frame has renewed for apiKey : " + apiName);
+			if (rateLimitModel == null) {
 
-			rateLimitModel.currentCounter = 1;
-			rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
-			rateLimitModel.nextFrame = getNextTimeFrame(apiName, defaultRate);
-			rateLimitModel.suspended = false;
-			return true;
+				log.info(" Api rate not configured for apiKey : " + apiName + ". So falling back to default rate : "
+						+ defaultRate);
 
-		} else {
+				rateLimitModel = new RateLimitModel();
+				rateLimitModel.currentCounter = 1;
+				rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
+				rateLimitModel.nextFrame = getNextTimeFrame(apiName, defaultRate);
+				runtimeConfig.put(apiName, rateLimitModel);
+				return true;
 
-			if (rateLimitModel.currentCounter + 1 <= rateLimitModel.counter) {
-				log.info(" Api called within the time frame for apiKey : " + apiName
-						+ ". So incrementing the counter for the api key.");
+			}
 
-				rateLimitModel.currentCounter++;
+			if (rateLimitModel.nextFrame.before(currentTimestamp)) {
+
+				log.info(" Api called after the time frame has renewed for apiKey : " + apiName);
+
+				rateLimitModel.currentCounter = 1;
+				rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
+				rateLimitModel.nextFrame = getNextTimeFrame(apiName, defaultRate);
+				rateLimitModel.suspended = false;
 				return true;
 
 			} else {
 
-				if (rateLimitModel.suspended) {
-					log.info(" Api called when in SUSPENDED state for apiKey : " + apiName
-							+ ". So blocking the call to the api.");
+				if (rateLimitModel.currentCounter + 1 <= rateLimitModel.counter) {
+					log.info(" Api called within the time frame for apiKey : " + apiName
+							+ ". So incrementing the counter for the api key.");
+
+					rateLimitModel.currentCounter++;
+					return true;
+
+				} else {
+
+					if (rateLimitModel.suspended) {
+						log.info(" Api called when in SUSPENDED state for apiKey : " + apiName
+								+ ". So blocking the call to the api.");
+
+						return false;
+					}
+					rateLimitModel.currentCounter = getMaxCounter(apiName, defaultRate);
+					rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
+					rateLimitModel.nextFrame = new Timestamp(
+							System.currentTimeMillis() + suspensionMinutes * 60 * 1000);
+					rateLimitModel.suspended = true;
+
+					log.info(" Api counter has exceeded the permissible value for apiKey : " + apiName
+							+ ". So suspending the api key for " + suspensionMinutes + " minutes.");
 
 					return false;
+
 				}
-				rateLimitModel.currentCounter = getMaxCounter(apiName, defaultRate);
-				rateLimitModel.counter = getMaxCounter(apiName, defaultRate);
-				rateLimitModel.nextFrame = new Timestamp(System.currentTimeMillis() + suspensionMinutes * 60 * 1000);
-				rateLimitModel.suspended = true;
-
-				log.info(" Api counter has exceeded the permissible value for apiKey : " + apiName
-						+ ". So suspending the api key for " + suspensionMinutes + " minutes.");
-
-				return false;
-
 			}
 		}
 	}
 
-	public int getMaxCounter(String apiName, String defaultRate) {
+	private int getMaxCounter(String apiName, String defaultRate) {
 
 		String rateString = StringUtils.isEmpty(apiRateMap.get(apiName)) ? defaultRate : apiRateMap.get(apiName);
 		String[] parts = rateString.split("[A-Z]");
@@ -122,7 +126,7 @@ public class RateLimitConfiguration {
 
 	}
 
-	public Timestamp getNextTimeFrame(String apiName, String defaultRate) {
+	private Timestamp getNextTimeFrame(String apiName, String defaultRate) {
 
 		String rateString = StringUtils.isEmpty(apiRateMap.get(apiName)) ? defaultRate : apiRateMap.get(apiName);
 		String[] parts = rateString.split("[A-Z]");
